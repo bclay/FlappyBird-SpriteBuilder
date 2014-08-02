b88b01138e40fafb0afec34860c685664f09ea3d
Brynn Claypoole <bclay@seas.upenn.edu>
2014-08-02 18:06:19 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,13 @@
 - (void)initialize
 {
     // your code here
+    //creates a character object (the actually flappy bird)
+    //loads a new character from the CCB file
+    //CCB files created by SpriteBuilder
+    character = (Character*)[CCBReader load:@"Character"];
+    //calling a method:
+    //[objectName methodName:parameter]
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
@@ -15,5 +22,11 @@
 }
 
 // put new methods here
+//declaring new methods:
+//-(type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName: (type of second parameter) secondParameter
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
 
 @end
